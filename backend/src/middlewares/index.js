f00a897909fed7814eed9286976fc2d434258c06
mfcastilho <fredericocastilho@hotmail.com>
2023-09-20 03:46:38 -0300

    feat: insert all validations middlewares at the index

--- backend/src/middlewares/index.js
+++ backend/src/middlewares/index.js
@@ -1,7 +1,13 @@
 const verifyFields = require("./verifyFields");
 const verifyIfEmailNotExists = require("./verifyIfEmailNotExists");
+const verifyEmail = require("./verifyEmail");
+const verifyLoginPassword = require("./verifyLoginPassword");
+const verifyToken = require("./verifyToken");
 
 module.exports = {
     verifyFields,
-    verifyIfEmailNotExists
+    verifyIfEmailNotExists,
+    verifyEmail,
+    verifyLoginPassword,
+    verifyToken
 }
