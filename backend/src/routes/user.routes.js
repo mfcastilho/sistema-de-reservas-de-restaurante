f01b0631f45c31d86688fead2b0f02cec7d26f1b
mfcastilho <fredericocastilho@hotmail.com>
2023-09-20 03:43:50 -0300

    feat: import at user routes all validations middlewares

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,10 +1,15 @@
 const {  Router } = require("express");
 const userRoutes = Router();
 const { CreateUserController, LoginUserController } = require("../controllers/index");
-const { verifyFields, verifyIfEmailNotExists } = require("../middlewares/index");
+const { verifyFields, 
+        verifyIfEmailNotExists, 
+        verifyLoginPassword,
+        verifyEmail,
+        verifyToken} = require("../middlewares/index");
 
 
-userRoutes.post("/users", verifyFields, CreateUserController.handle);
-userRoutes.post("/login", verifyIfEmailNotExists, LoginUserController.handle);
+userRoutes.post("/users", verifyFields, verifyEmail, CreateUserController.handle);
+userRoutes.post("/login", verifyFields, verifyIfEmailNotExists, verifyLoginPassword, LoginUserController.handle);
+
 
 module.exports = userRoutes;
