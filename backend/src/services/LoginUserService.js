1be29b50435478e33d3df9d7855848ea78922dd1
mfcastilho <fredericocastilho@hotmail.com>
2023-09-20 02:52:30 -0300

    feat: implement LoginUserService

---
+++ backend/src/services/LoginUserService.js
@@ -0,0 +1,21 @@
+const { usersRepository } = require("../repositories/index");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+class LoginUserService {
+    async execute(userData) {
+        
+            const { email } = userData;
+            const repo = usersRepository;
+
+            const user = await repo.findOne({where: {email}, raw: true});
+
+            const token = jwt.sign({id: user.id}, process.env.JWT_SECRET_KEY, {expiresIn: process.env.JWT_EXPIRES_IN});
+
+            const { password, ...userLogged } = user;
+
+            return { user: userLogged, token }; 
+    }
+}
+
+module.exports = LoginUserService;
---
+++ backend/src/services/index.js
@@ -0,0 +1,8 @@
+const CreateUserService = require("./CreateUserService");
+const LoginUserService = require("./LoginUserService");
+
+
+module.exports = {
+    CreateUserService,
+    LoginUserService
+}
