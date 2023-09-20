a46aec86fd40040bd946bc1eb2772131e369e47e
mfcastilho <fredericocastilho@hotmail.com>
2023-09-20 00:15:42 -0300

    feat: create index file to agroupe the repositories

---
+++ backend/src/repositories/index.js
@@ -0,0 +1,10 @@
+const reservationsRepository = require("../repositories/reservations.repository");
+const tablesRepository = require("../repositories/tables.repository");
+const usersRepository = require("../repositories/users.repository");
+
+
+module.exports = {
+    reservationsRepository,
+    tablesRepository,
+    usersRepository
+}
