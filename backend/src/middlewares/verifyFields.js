a309c78203d9f387241dbc380dabafa814ca9826
mfcastilho <fredericocastilho@hotmail.com>
2023-09-20 03:49:02 -0300

    update: transform the verifyFields in a generic validator, include the url verifications to know  whats is  the endpoint to validate the fields came from req.body

--- backend/src/middlewares/verifyFields.js
+++ backend/src/middlewares/verifyFields.js
@@ -2,11 +2,26 @@
 
 
 const verifyFields = (req, res, next)=>{
-    const { name, email, password } = req.body;
 
-    if(!name || !email || !password) return res.status(400).json({error: "Todos os campos são obrigatórios."});
+    console.log(req.url);
 
-    return next();
+    if(req.url == "/login"){
+        const { email, password } = req.body;
+
+        if(!email || !password) return res.status(400).json({error: "Todos os campos são obrigatórios."});
+
+        return next();
+    }
+
+    if(req.url == "/users"){
+        const { name, email, password } = req.body;
+
+        if(!name || !email || !password) return res.status(400).json({error: "Todos os campos são obrigatórios."});
+
+        return next();
+    }
+
+    
 }
 
 
