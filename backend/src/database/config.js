8f820ae1028ce036227b246642472db9634d2adc
mfcastilho <fredericocastilho@hotmail.com>
2023-09-19 21:25:07 -0300

    config: configuring config.js file

--- backend/src/database/config.js
+++ backend/src/database/config.js
@@ -1,23 +1,30 @@
-{
+require("dotenv").config();
+
+
+module.exports = {
+
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DEV_DB_USER,
+    "password":process.env.DEV_DB_PASS,
+    "database":process.env.DEV_DB_NAME,
+    "host":process.env.DEV_DB_HOST,
+    "dialect":process.env.DEV_DB_DIALECT 
   },
+
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username":process.env.DEV_DB_USER,
+    "password":process.env.DEV_DB_PASS,
+    "database":process.env.DEV_DB_NAME,
+    "host":process.env.DEV_DB_HOST,
+    "dialect":process.env.DEV_DB_DIALECT
   },
+
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.PROD_DB_USER,
+    "password": process.env.PROD_DB_PASS,
+    "database": process.env.PROD_DB_NAME,
+    "host": process.env.PROD_DB_HOST,
+    "dialect": process.env.PROD_DB_DIALECT
   }
+  
 }
