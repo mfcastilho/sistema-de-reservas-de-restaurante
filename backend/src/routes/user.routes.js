b357ab31111edcb97dc0da615ed328879384513d
mfcastilho <fredericocastilho@hotmail.com>
2023-09-20 01:04:39 -0300

    feat: implement CreateUser route

---
+++ backend/src/routes/user.routes.js
@@ -0,0 +1,8 @@
+const {  Router } = require("express");
+const userRoutes = Router();
+const { CreateUserController } = require("../controllers/index");
+
+
+userRoutes.post("/users", CreateUserController.handle);
+
+module.exports = userRoutes;
