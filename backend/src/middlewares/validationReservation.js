4553c2647730adcce8bdf6a299b1fa4418e0946e
mfcastilho <fredericocastilho@hotmail.com>
2023-09-21 03:17:36 -0300

    feat: create validationReservation middleware

---
+++ backend/src/middlewares/validationReservation.js
@@ -0,0 +1,32 @@
+const { isWithinInterval, isSunday } = require('date-fns');
+
+async function validateReservationMiddleware(req, res, next) {
+  try {
+    const { date, hour } = req.body; 
+
+    const dateHourReservation = new Date(`${date}T${hour}:00`);
+
+    const startInterval = new Date(dateHourReservation);
+    startInterval.setHours(18, 0, 0, 0);
+
+    const endInterval = new Date(dateHourReservation);
+    endInterval.setHours(23, 59, 59, 999);
+
+    if (!isWithinInterval(dateHourReservation, { start: startInterval, end: endInterval })) {
+      return res.status(400).json({error: 'Reservas permitidas apenas das 18:00 até 23:59, exceto aos domingos.'});
+    }
+
+    if (isSunday(dateHourReservation)) {
+        return res.status(400).json({error: 'Reservas não são permitidas aos domingos.'});
+    }
+
+
+    return next();
+
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error: "Erro interno do servidor."});
+  }
+}
+
+module.exports = validateReservationMiddleware;
