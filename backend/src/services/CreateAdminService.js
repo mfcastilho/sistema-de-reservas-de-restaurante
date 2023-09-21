03208ae8a46310f203798bf601421cf565f9f46c
mfcastilho <fredericocastilho@hotmail.com>
2023-09-20 22:54:27 -0300

    feat: create CreateAdminService

---
+++ backend/src/services/CreateAdminService.js
@@ -0,0 +1,35 @@
+const { usersRepository } = require("../repositories/index");
+const { v4:makeId } = require("uuid");
+const bcrypt = require("bcrypt");
+
+
+
+class CreateUserService {
+    async execute(userData) {
+        try {
+            const { name, email, password, role } = userData;
+            const repo = usersRepository;
+
+            const cryptedPassword = await bcrypt.hash(password, 10);
+
+            const newUser = {
+                id: makeId(),
+                name,
+                email,
+                password: cryptedPassword,
+                role
+            }
+
+            await repo.create(newUser);
+
+            delete newUser.password;
+
+            return newUser;
+            
+        } catch (error) {
+            throw new Error("Erro interno do servidor.");
+        }
+    }
+}
+
+module.exports = CreateUserService;
