9831dd38778ead24a8c0fc56fa1d18382dd34bc0
mfcastilho <fredericocastilho@hotmail.com>
2023-09-20 23:02:26 -0300

    update: insert the verifyAdminRegistration at the index middleware

--- backend/src/middlewares/index.js
+++ backend/src/middlewares/index.js
@@ -3,11 +3,15 @@ const verifyIfEmailNotExists = require("./verifyIfEmailNotExists");
 const verifyEmail = require("./verifyEmail");
 const verifyLoginPassword = require("./verifyLoginPassword");
 const verifyToken = require("./verifyToken");
+const verifyAdminRegistration = require("./verifyAdminRegistration");
+
+
 
 module.exports = {
     verifyFields,
     verifyIfEmailNotExists,
     verifyEmail,
     verifyLoginPassword,
-    verifyToken
+    verifyToken,
+    verifyAdminRegistration
 }
