b4dfc0cf28b4d79faea6eab6fac6218e04736c2d
mfcastilho <fredericocastilho@hotmail.com>
2023-09-20 22:58:14 -0300

    feat: create CreateAdminController

---
+++ backend/src/controllers/CreateAdminController.js
@@ -0,0 +1,22 @@
+const { CreateUserService } = require("../services/index");
+
+class CreateUserController {
+    async handle(req, res) {
+        try {
+            const { name, email, password } = req.body;
+
+            const service = new CreateUserService();
+
+            const role = "admin";
+
+            const userCreated = await service.execute({ name, email, password, role });
+
+            return res.status(201).json(userCreated);
+
+        } catch (error) {
+            return res.status(500).json({error: "Erro interno do servidor."});
+        }
+    }
+}
+
+module.exports = new CreateUserController;
--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -1,9 +1,12 @@
+const CreateAdminController = require("./CreateAdminController");
 const CreateUserController = require("./CreateUserController");
 const GetAllTablesController = require("./GetAllTablesController");
 const LoginUserController = require("./LoginUserController");
 
+
 module.exports = {
     CreateUserController,
     LoginUserController,
-    GetAllTablesController
+    GetAllTablesController,
+    CreateAdminController
 }
