8db01c494f6dd58e2ce2cc8c073387747b359fb6
mfcastilho <fredericocastilho@hotmail.com>
2023-09-20 22:57:41 -0300

    feat: create CreateAdminService

--- backend/src/services/index.js
+++ backend/src/services/index.js
@@ -1,11 +1,11 @@
 const CreateUserService = require("./CreateUserService");
 const GetAllTablesService = require("./GetAllTablesService");
 const LoginUserService = require("./LoginUserService");
-
-
+const CreateAdminService = require("./CreateAdminService");
 
 module.exports = {
     CreateUserService,
     LoginUserService,
-    GetAllTablesService
+    GetAllTablesService,
+    CreateAdminService
 }
