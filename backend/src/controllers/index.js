7139498d66bb2c8b3a2004b1fa3a8a1960c22c40
mfcastilho <fredericocastilho@hotmail.com>
2023-09-21 03:22:26 -0300

    feat: insert CreateReservationController at the index controller

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -2,11 +2,12 @@ const CreateAdminController = require("./CreateAdminController");
 const CreateUserController = require("./CreateUserController");
 const GetAllTablesController = require("./GetAllTablesController");
 const LoginUserController = require("./LoginUserController");
-
+const CreateReservationController = require("./CreateReservationController");
 
 module.exports = {
     CreateUserController,
     LoginUserController,
     GetAllTablesController,
-    CreateAdminController
+    CreateAdminController,
+    CreateReservationController
 }
