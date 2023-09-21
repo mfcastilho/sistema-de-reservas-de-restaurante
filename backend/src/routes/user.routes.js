d0e6066b446dd0a65599ff570ee249bc25e79b8c
mfcastilho <fredericocastilho@hotmail.com>
2023-09-20 23:03:29 -0300

    feat: create the admin and client registration endpoints

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,14 +1,16 @@
 const {  Router } = require("express");
 const userRoutes = Router();
-const { CreateUserController, LoginUserController } = require("../controllers/index");
+const { CreateUserController, CreateAdminController, LoginUserController } = require("../controllers/index");
 const { verifyFields, 
         verifyIfEmailNotExists, 
         verifyLoginPassword,
         verifyEmail,
+        verifyAdminRegistration,
         verifyToken} = require("../middlewares/index");
 
 
-userRoutes.post("/users", verifyFields, verifyEmail, CreateUserController.handle);
+userRoutes.post("/register/client", verifyFields, verifyEmail, CreateUserController.handle);
+userRoutes.post("/register/admin", verifyFields, verifyAdminRegistration, CreateAdminController.handle);
 userRoutes.post("/login", verifyFields, verifyIfEmailNotExists, verifyLoginPassword, LoginUserController.handle);
 
 
