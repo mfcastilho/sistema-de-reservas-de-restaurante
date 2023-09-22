0bf20254dafa4ea0f0a8790145e79b55c2f05fd6
mfcastilho <fredericocastilho@hotmail.com>
2023-09-22 04:31:28 -0300

    update: organize the code imports at the user.routes

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,16 +1,18 @@
 const {  Router } = require("express");
 const userRoutes = Router();
+
 const { CreateUserController, CreateAdminController, LoginUserController } = require("../controllers/index");
 const { verifyFields, 
         verifyIfEmailNotExists, 
         verifyLoginPassword,
         verifyEmail,
-        verifyAdminRegistration,
-        verifyToken} = require("../middlewares/index");
+        verifyAdminRegistration } = require("../middlewares/index");
 
 
 userRoutes.post("/register/client", verifyFields, verifyEmail, CreateUserController.handle);
+
 userRoutes.post("/register/admin", verifyFields, verifyAdminRegistration, CreateAdminController.handle);
+
 userRoutes.post("/login", verifyFields, verifyIfEmailNotExists, verifyLoginPassword, LoginUserController.handle);
 
 
