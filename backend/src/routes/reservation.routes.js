ac5a887e13e3a31d3c8b138bb684114456eb0c24
mfcastilho <fredericocastilho@hotmail.com>
2023-09-21 23:29:04 -0300

    feat: create GetAllUserReservation route

--- backend/src/routes/reservation.routes.js
+++ backend/src/routes/reservation.routes.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
 const reservationRoutes = Router();
-const { CreateReservationController } = require("../controllers/index");
+const { CreateReservationController, GetAllUserReservationsController } = require("../controllers/index");
 
 const { checkTableAvailability, validationReservation, verifyToken } = require("../middlewares/index");
 
 
 reservationRoutes.post("/reservation", verifyToken, validationReservation, checkTableAvailability, CreateReservationController.handle);
+reservationRoutes.get("/reservations/user", verifyToken, GetAllUserReservationsController.handle);
 
 
 module.exports = reservationRoutes;
