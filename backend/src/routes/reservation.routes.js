d2828ed0a03e9aca9771b9ed47c94a945ab041f7
mfcastilho <fredericocastilho@hotmail.com>
2023-09-22 04:30:40 -0300

    feat: create GetAllSystemReservation and DeleClientReservation routes

--- backend/src/routes/reservation.routes.js
+++ backend/src/routes/reservation.routes.js
@@ -1,12 +1,27 @@
 const { Router } = require("express");
 const reservationRoutes = Router();
-const { CreateReservationController, GetAllUserReservationsController, DeleteReservationController } = require("../controllers/index");
 
-const { checkTableAvailability, validationReservation, verifyToken } = require("../middlewares/index");
+const { CreateReservationController, 
+        GetAllUserReservationsController, 
+        DeleteReservationController, 
+        GetAllSystemReservationsController,
+        DeleteClientReservationController } = require("../controllers/index");
+        
+const { checkTableAvailability, 
+        validationReservation, 
+        verifyToken, 
+        verifyIfIsAdmin } = require("../middlewares/index");
 
+
+
+reservationRoutes.get("/admin/all-reservations", verifyToken, verifyIfIsAdmin, GetAllSystemReservationsController.handle);
+
+reservationRoutes.delete("/admin/reservation/:id", verifyToken, verifyIfIsAdmin, DeleteClientReservationController.handle);
 
 reservationRoutes.post("/reservation", verifyToken, validationReservation, checkTableAvailability, CreateReservationController.handle);
+
 reservationRoutes.get("/reservations/user", verifyToken, GetAllUserReservationsController.handle);
+
 reservationRoutes.delete("/reservation/:id", verifyToken, DeleteReservationController.handle);
 
 
