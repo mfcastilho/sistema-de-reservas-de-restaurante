859ba55c1284ba7b3f69b172ed25215ac3443dd3
mfcastilho <fredericocastilho@hotmail.com>
2023-09-22 00:28:16 -0300

    feat: create DeleteReservation route

--- backend/src/routes/reservation.routes.js
+++ backend/src/routes/reservation.routes.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 const reservationRoutes = Router();
-const { CreateReservationController, GetAllUserReservationsController } = require("../controllers/index");
+const { CreateReservationController, GetAllUserReservationsController, DeleteReservationController } = require("../controllers/index");
 
 const { checkTableAvailability, validationReservation, verifyToken } = require("../middlewares/index");
 
 
 reservationRoutes.post("/reservation", verifyToken, validationReservation, checkTableAvailability, CreateReservationController.handle);
 reservationRoutes.get("/reservations/user", verifyToken, GetAllUserReservationsController.handle);
+reservationRoutes.delete("/reservation/:id", verifyToken, DeleteReservationController.handle);
 
 
 module.exports = reservationRoutes;
