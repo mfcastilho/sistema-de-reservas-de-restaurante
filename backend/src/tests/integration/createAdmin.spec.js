e94915db011515d31c878bbfd6f7b00101c6ab5a
mfcastilho <fredericocastilho@hotmail.com>
2023-09-23 01:35:34 -0300

    test: add createAdmin test

---
+++ backend/src/tests/integration/createAdmin.spec.js
@@ -0,0 +1,63 @@
+
+
+
+
+
+const request = require("supertest");
+const app = require("../../app");
+const connection = require("../../models/index");
+const truncate = require("./truncate");
+
+describe("Create Admin", ()=>{
+    afterAll(()=>{
+        connection.sequelize.close();
+    });
+
+    beforeEach(async () => {
+        await truncate(connection.sequelize.models);
+    });
+
+    it("É possível criar um admnistrador", async ()=>{
+        const response = await request(app).post("/api/v1/register/admin").send({
+            name: "Mario",
+            email: "mario@email.com",
+            password: "123"
+        });
+
+        expect(response.ok).toBeTruthy();
+        expect(response.body).toHaveProperty("id");
+    });
+
+
+    it("Não é possível cadastrar o admnistrador se algum campo estiver vazio", async ()=>{
+        const response = await request(app).post("/api/v1/register/admin").send({
+            name: "",
+            email: "mario@email.com",
+            password: "123"
+        });
+
+        expect(response.ok).toBeFalsy();
+        expect(response.body).toHaveProperty("error");
+        expect(response.body.error).toEqual("Todos os campos são obrigatórios.");
+    });
+
+    it("Não é possível cadastrar o admnistrador com email existente", async ()=>{
+        let response = await request(app).post("/api/v1/register/admin").send({
+            name: "Mario Frederico",
+            email: "mario@email.com",
+            password: "123"
+        });
+
+        response = await request(app).post("/api/v1/register/admin").send({
+            name: "Mario da Silva",
+            email: "mario@email.com",
+            password: "123"
+        });
+
+        expect(response.ok).toBeFalsy();
+        expect(response.body).toHaveProperty("error")
+        expect(response.body.error).toEqual("Já existe um administrador cadastrado com o e-mail informado.");
+    });
+
+    
+});
