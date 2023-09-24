05676a35c512b06e29f8529990dd6d17cfca9d0b
mfcastilho <fredericocastilho@hotmail.com>
2023-09-24 02:58:39 -0300

    update: insert the reservation url at the verityFields

--- backend/src/middlewares/verifyFields.js
+++ backend/src/middlewares/verifyFields.js
@@ -3,7 +3,7 @@
 
 const verifyFields = (req, res, next)=>{
 
-    if(req.url == "/login"){
+    if(req.url === "/login"){
         const { email, password } = req.body;
 
         if(!email || !password) return res.status(400).json({error: "Todos os campos são obrigatórios."});
@@ -11,17 +11,26 @@ const verifyFields = (req, res, next)=>{
         return next();
     }
 
-    if(req.url == "/register/client" || req.url == "/register/admin"){
+    if(req.url === "/register/client" || req.url == "/register/admin"){
         const { name, email, password } = req.body;
 
         if(!name || !email || !password) return res.status(400).json({error: "Todos os campos são obrigatórios."});
 
         return next();
     }
-    if(req.url == "/table"){
+
+    if(req.url === "/table"){
         const { table_number, capacity, is_available } = req.body;
 
         if(!table_number || !capacity || !is_available) return res.status(400).json({error: "Todos os campos são obrigatórios."});
+
+        return next();
+    }
+
+    if(req.url === "/reservation"){
+        const { table_number, date, hour } = req.body;
+
+        if(!table_number || !date || !hour) return res.status(400).json({error: "Todos os campos são obrigatórios."});
 
         return next();
     }
