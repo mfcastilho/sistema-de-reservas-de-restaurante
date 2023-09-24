0de5dec03c3e348c88abf6e132deb4e3ccd1e60e
mfcastilho <fredericocastilho@hotmail.com>
2023-09-23 22:06:38 -0300

    config: add sequelize.js file at the config folder to create the Sequelize instance

---
+++ backend/src/config/sequelize.js
@@ -0,0 +1,30 @@
+// config/database.js
+const { Sequelize } = require('sequelize');
+
+const environment = process.env.NODE_ENV || 'development';
+
+let sequelize;
+
+if (environment === 'production') {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    logging: false,
+  });
+} else if (environment === 'test') {
+  sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: ':memory:',
+    logging: false,
+  });
+} else {
+  sequelize = new Sequelize({
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'seu-usuario',
+    password: 'sua-senha',
+    database: 'nome-do-banco',
+    logging: false,
+  });
+}
+
+module.exports = sequelize;
