1cfcf24d4ba59fd9eac5f8cd0d8cc3712a4a03bb
mfcastilho <fredericocastilho@hotmail.com>
2023-09-23 22:09:11 -0300

    config: insert the sqlite memory database config

--- backend/src/config/database/config.js
+++ backend/src/config/database/config.js
@@ -1,41 +1,35 @@
 require("dotenv").config();
 
-
 module.exports = {
-
-  "development": {
-    "username": process.env.DEV_DB_USER,
-    "password":process.env.DEV_DB_PASS,
-    "database":process.env.NODE_ENV === "test" ? "db_restaurante_test" : process.env.DEV_DB_NAME,
-    "host":process.env.DEV_DB_HOST,
-    "dialect":process.env.DEV_DB_DIALECT,
-    define: {
-        underscored: true,
-    }, 
-  },
-
-  "test": {
-    "username": process.env.DEV_DB_USER,
-    "password":process.env.DEV_DB_PASS,
-    "database":"db_restaurante_test",
-    "host":process.env.DEV_DB_HOST,
-    "dialect":process.env.DEV_DB_DIALECT,
-    define: {
-        underscored: true,
+    development: {
+      dialect: process.env.DEV_DB_DIALECT,
+      host: process.env.DEV_DB_HOST,
+      username: process.env.DEV_DB_USER,
+      password: process.env.DEV_DB_PASS,
+      database: process.env.DEV_DB_NAME,
+      logging: false,
+      define: {
+        underscored: true, 
+      }
     },
-  },
-
-  "production": {
-    "username": process.env.PROD_DB_USER,
-    "password": process.env.PROD_DB_PASS,
-    "database": process.env.PROD_DB_NAME,
-    "host": process.env.PROD_DB_HOST,
-    "dialect": process.env.PROD_DB_DIALECT,
-    define: {
-        underscored: true,
+    test: {
+      dialect: 'sqlite',
+      storage: ':memory:',
+      logging: false,
+      define: {
+        underscored: true, 
+      }
     },
-  }
+    production: {
+        dialect: process.env.PROD_DB_DIALECT,
+        host: process.env.PROD_DB_HOST,
+        username: process.env.PROD_DB_USER,
+        password: process.env.PROD_DB_PASS,
+        database: process.env.PROD_DB_NAME,
+        logging: false,
+        define: {
+          underscored: true, 
+        }
+    },
+  };
   
-}
-
-
