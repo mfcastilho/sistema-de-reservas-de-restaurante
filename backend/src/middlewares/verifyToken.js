4b95ee24389b576820fbe0ea4e496ac8ca88a7f6
mfcastilho <fredericocastilho@hotmail.com>
2023-09-24 03:05:10 -0300

    update: create re.decoded to receive decoded token infos

--- backend/src/middlewares/verifyToken.js
+++ backend/src/middlewares/verifyToken.js
@@ -6,7 +6,7 @@ const verifyToken = (req, res, next)=>{
 
     try {
         const { authorization } = req.headers;
-
+        
         if(!authorization) return res.status(401).json({error: "Para acessar este recurso um token de autenticação válido deve ser enviado."});
 
         const token = authorization.split(" ")[1];
@@ -14,6 +14,7 @@ const verifyToken = (req, res, next)=>{
         const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
         res.locals.decoded = decoded;
+        req.decoded = decoded;
 
         return next();
         
