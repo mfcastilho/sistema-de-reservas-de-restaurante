962673469ca8534601e79f7e4ab72c89d021d7aa
mfcastilho <fredericocastilho@hotmail.com>
2023-09-23 22:18:00 -0300

    feat: create CreateTable route

--- backend/src/routes/table.routes.js
+++ backend/src/routes/table.routes.js
@@ -1,9 +1,12 @@
 const { Router } = require("express");
 const tableRoutes = Router();
-const { GetAllTablesController } = require("../controllers/index");
+const { GetAllTablesController, CreateTableController } = require("../controllers/index");
+
+const { verifyFields } = require("../middlewares/index");
 
 
 tableRoutes.get("/tables", GetAllTablesController.handle);
+tableRoutes.post("/table", verifyFields, CreateTableController.handle);
 
 
 module.exports = tableRoutes;
