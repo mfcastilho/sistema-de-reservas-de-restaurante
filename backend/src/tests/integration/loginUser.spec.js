58e3315c6a81794b377be7bd15f4a1b0b4cb858c
mfcastilho <fredericocastilho@hotmail.com>
2023-09-23 22:13:09 -0300

    test: add loginUser test

--- backend/src/tests/integration/loginUser.spec.js
+++ backend/src/tests/integration/loginUser.spec.js
@@ -1,18 +1,15 @@
 const request = require("supertest");
 const app = require("../../app");
-const connection = require("../../models/index");
-const truncate = require("./truncate");
-const { User } = require("../../models");
+const { sequelize } = require("../../models");
 
-describe("User Login", ()=>{
+describe("User Login", () => {
 
-    afterAll(async ()=>{
-        // await User.destroy({where: {email: "davi@email.com"}});
-        connection.sequelize.close();
-    });
+    beforeAll(async () => {
+        process.env.NODE_ENV = 'test';
+        process.env.DATABASE_URL = 'sqlite::memory:';
 
-    beforeAll( async ()=>{
-        // await truncate(connection.sequelize.models);
+        await sequelize.sync({ force: true });
+
         await request(app).post("/api/v1/register/client").send({
             name: "davi",
             email: "davi@email.com",
@@ -20,53 +17,51 @@ describe("User Login", ()=>{
         });
     });
 
-    it("É possível fazer login", async ()=>{
+    afterAll(async () => {
+        await sequelize.close();
+    });
 
-        await request(app).post("/api/v1/register/client").send({
-            name: "davi",
-            email: "davi@email.com",
-            password: "123"
-        });
-        
-        let response = await request(app).post("/api/v1/login").send({
-            email: "davi@email.com",
-	        password: "123"
+    it("É possível fazer login", async () => {
+
+        const response = await request(app).post("/api/v1/login").send({
+        email: "davi@email.com",
+        password: "123"
         });
 
         expect(response.ok).toBeTruthy();
-        expect(response.body).toHaveProperty("token");
+        expect(response.body).toHaveProperty("token")
     });
 
-    it("Não é possível o usuário fazer login se algum campo estiver vazio", async ()=>{
 
-        let response = await request(app).post("/api/v1/login").send({
-            email: "",
-	        password: "123"
+    it("Não é possível o usuário fazer login se algum campo estiver vazio", async () => {
+        const response = await request(app).post("/api/v1/login").send({
+          email: "",
+          password: "123"
         });
-
+  
         expect(response.ok).toBeFalsy();
         expect(response.body).toHaveProperty("error");
         expect(response.body.error).toEqual("Todos os campos são obrigatórios.");
-
     });
 
-    it("Não é possível o usuário fazer se o email for inválido", async ()=>{
-        let response = await request(app).post("/api/v1/login").send({
-            email: "davi25@email.com",
-	        password: "123"
+    it("Não é possível o usuário fazer login se o email for inválido", async () => {
+        const response = await request(app).post("/api/v1/login").send({
+          email: "davi25@email.com",
+          password: "123"
         });
-
+  
         expect(response.ok).toBeFalsy();
         expect(response.body).toHaveProperty("error");
         expect(response.body.error).toEqual("Email e/ou senha incorretos.");
     });
 
-    it("Não é possível o usuário fazer se a senha for inválida", async ()=>{
-        let response = await request(app).post("/api/v1/login").send({
-            email: "davi25@email.com",
-	        password: "12345"
-        });
 
+    it("Não é possível o usuário fazer login se a senha for inválida", async () => {
+        const response = await request(app).post("/api/v1/login").send({
+          email: "davi25@email.com",
+          password: "12345"
+        });
+  
         expect(response.ok).toBeFalsy();
         expect(response.body).toHaveProperty("error");
         expect(response.body.error).toEqual("Email e/ou senha incorretos.");
