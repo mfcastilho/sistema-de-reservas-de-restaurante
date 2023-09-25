53d2eb1151455e741ef1452565fe446d77991cf7
mfcastilho <fredericocastilho@hotmail.com>
2023-09-25 01:07:30 -0300

    feat: insert RoutsApp component at the App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
-import './App.css'
+import './App.css';
+import RoutesApp from "../RoutesApp"
 
 function App() {
 
 
-  return (
-    <div className='app'>
-      
-    </div>
-  )
+    return (
+        <div className='app'>
+            <RoutesApp/>
+        </div>
+    )
 }
 
 export default App
