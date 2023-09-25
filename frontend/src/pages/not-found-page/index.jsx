53f375a014bc32d39d6d44ac8b3b06769ca2616a
mfcastilho <fredericocastilho@hotmail.com>
2023-09-25 16:39:18 -0300

    wip: create base structure NotFoundPage component

---
+++ frontend/src/pages/not-found-page/index.jsx
@@ -0,0 +1,14 @@
+import './style.css';
+
+const NotFound = () => {
+  return (
+    <div className="not-found-container">
+      <h1 className="not-found-heading">Página Não Encontrada</h1>
+      <p className="not-found-message">
+        Desculpe, a página que você está procurando não existe.
+      </p>
+    </div>
+  );
+};
+
+export default NotFound;
