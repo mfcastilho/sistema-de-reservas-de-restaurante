92c02f23c80808d8e5ca88383deca9892261098a
mfcastilho <fredericocastilho@hotmail.com>
2023-09-25 16:50:13 -0300

    feat: insert all the system pages at the RoutesApp component

--- frontend/RoutesApp.jsx
+++ frontend/RoutesApp.jsx
@@ -1,39 +1,45 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 
 import LoginPage from "./src/pages/login-page/index";
 import ReservationPage from "./src/pages/reservation-page/index";
 import ClienteRegistrationPage from "./src/pages/cliente-registration";
 import AdminRegistrationPage from "./src/pages/admin-registration";
 import HomePage from "./src/pages/home-page";
+import Unauthorized from "./src/pages/unauthorized-page";
+import Error500 from "./src/pages/generic-error-page";
+import AdminPanelPage from "./src/pages/admin-panel-page";
+import NotFound from "./src/pages/not-found-page";
+
 
 import PrivateRoute from "./PrivateRoute";
 
 function RoutesApp() {
     
-  // eslint-disable-next-line no-unused-vars
-  const [authenticated, setAuthenticated] = useState(false);
-  const [user, setUser] = useState(null);
-
+ 
   return (
-    <BrowserRouter>
-      <Routes>
 
-        <Route path="/" element={<HomePage/>} />
+        <Routes>
+            
+            <Route path="/" element={<HomePage/>} />
 
-        <Route
-          path="/login"
-          element={<LoginPage setAuthenticated={setAuthenticated} setUser={setUser} />}
-        />
-        <Route path="register/client" element={<ClienteRegistrationPage/>} />
+            <Route path="/login" element={<LoginPage />} />
 
-        <Route path="register/admin" element={<AdminRegistrationPage/>} />
+            <Route path="register/client" element={<ClienteRegistrationPage/>} />
 
-        <Route element={<PrivateRoute/>}>
-            <Route path="/reservation" element={<ReservationPage user={user}/>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+            <Route path="register/admin" element={<AdminRegistrationPage/>} />
+
+            <Route path="/error" element={<Error500/>} />
+            
+            <Route path="/unauthorized" element={<Unauthorized/>} />
+
+            <Route path="*" element={<NotFound/>} />
+
+            <Route element={<PrivateRoute/>}>
+                <Route path="/admin" element={<AdminPanelPage/>} />
+                <Route path="/reservation" element={<ReservationPage/>} />
+            </Route>
+        </Routes>
+
   );
 }
 
