009d922175db06399472ef4295d653260c88c216
mfcastilho <fredericocastilho@hotmail.com>
2023-09-25 16:43:42 -0300

    feat: insert the PrivateRoute component at the RoutesApp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css';
-import RoutesApp from "../RoutesApp"
+import RoutesApp from "../RoutesApp";
+import Header from './components/header';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from "./components/auth-provider"
 
 function App() {
 
 
     return (
-        <div className='app'>
-            <RoutesApp/>
-        </div>
+        <AuthProvider>
+            <BrowserRouter>
+                <div className='app'>
+                    <Header /> 
+                    <RoutesApp />
+                </div>
+            </BrowserRouter>
+        </AuthProvider>
+        
     )
 }
 
