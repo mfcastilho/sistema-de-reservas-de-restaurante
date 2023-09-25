9b3a88be0539f05f10a1a4132099ca3429f5ad6c
mfcastilho <fredericocastilho@hotmail.com>
2023-09-25 04:30:45 -0300

    wip: insert referential title

---
+++ frontend/src/pages/home-page/index.jsx
@@ -0,0 +1,16 @@
+
+
+
+const HomePage = ()=>{
+
+
+    return(
+        <div className="home">
+            <div className="home__container">
+                Home Page
+            </div>
+        </div>
+    );
+}
+
+export default HomePage;
