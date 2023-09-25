609e1f69034befd1bca6ae12ec6abaf07a5ce00f
mfcastilho <fredericocastilho@hotmail.com>
2023-09-25 04:28:55 -0300

    wip: create base structure RoutesApp component

--- frontend/RoutesApp.jsx
+++ frontend/RoutesApp.jsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 
 import LoginPage from "./src/pages/login-page/index";
 import ReservationPage from "./src/pages/reservation-page/index";
+import ClienteRegistrationPage from "./src/pages/cliente-registration";
+import AdminRegistrationPage from "./src/pages/admin-registration";
+import HomePage from "./src/pages/home-page";
 
 import PrivateRoute from "./PrivateRoute";
 
@@ -15,10 +18,17 @@ function RoutesApp() {
   return (
     <BrowserRouter>
       <Routes>
+
+        <Route path="/" element={<HomePage/>} />
+
         <Route
           path="/login"
           element={<LoginPage setAuthenticated={setAuthenticated} setUser={setUser} />}
         />
+        <Route path="register/client" element={<ClienteRegistrationPage/>} />
+
+        <Route path="register/admin" element={<AdminRegistrationPage/>} />
+
         <Route element={<PrivateRoute/>}>
             <Route path="/reservation" element={<ReservationPage user={user}/>} />
         </Route>
