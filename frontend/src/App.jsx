e3f2385dcf4dddb8ce3009c1f502e4bceb8d6235
mfcastilho <fredericocastilho@hotmail.com>
2023-09-25 23:44:20 -0300

    feat: insert the Footer component at the App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import RoutesApp from "../RoutesApp";
 import Header from './components/header';
+import Footer from './components/footer';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from "./components/auth-provider"
 
@@ -13,6 +14,7 @@ function App() {
                 <div className='app'>
                     <Header /> 
                     <RoutesApp />
+                    <Footer/>
                 </div>
             </BrowserRouter>
         </AuthProvider>
