8ef6a01a7911582f4a24c12dd9e07f39267ebed8
mfcastilho <fredericocastilho@hotmail.com>
2023-09-25 23:27:16 -0300

    feat: create UnauthorizedPage

--- frontend/src/pages/unauthorized-page/index.jsx
+++ frontend/src/pages/unauthorized-page/index.jsx
@@ -1,6 +1,29 @@
 import './style.css';
+import { useAuth } from "../../components/auth-provider";
+import jwtDecode from 'jwt-decode';
+import { useEffect } from 'react';
 
 const Unauthorized = () => {
+
+    const { login, logout } = useAuth(); 
+    const token = localStorage.getItem("token");
+    
+
+    const verifyIfIsLogged = ()=>{
+        if(token){
+            const decodedToken = jwtDecode(token);
+            if(decodedToken.exp * 1000 > Date.now()){
+                login();
+            }
+        }else{
+            logout();
+        }   
+    } 
+
+    useEffect(()=>{
+        verifyIfIsLogged();
+    },[]);
+
   return (
     <div className="unauthorized-container">
       <h1 className="unauthorized-heading">Não autorizado</h1>
