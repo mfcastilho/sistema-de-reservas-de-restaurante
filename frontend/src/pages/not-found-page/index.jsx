2e1b7b595401b59b3a2c5293114813d668acc787
mfcastilho <fredericocastilho@hotmail.com>
2023-09-25 23:27:47 -0300

    feat: create NotFoundPage

--- frontend/src/pages/not-found-page/index.jsx
+++ frontend/src/pages/not-found-page/index.jsx
@@ -1,6 +1,31 @@
 import './style.css';
+import { useAuth } from "../../components/auth-provider";
+import jwtDecode from 'jwt-decode';
+import { useEffect } from 'react';
 
 const NotFound = () => {
+
+    const { login, logout } = useAuth(); 
+    
+
+    const verifyIfIsLogged = ()=>{
+        const token = localStorage.getItem("token");
+        
+        if(token){
+            const decodedToken = jwtDecode(token);
+            if(decodedToken.exp * 1000 > Date.now()){
+                login();
+            }
+        }else{
+            logout();
+        }   
+    } 
+
+    useEffect(()=>{
+        verifyIfIsLogged();
+    },[]);
+
+
   return (
     <div className="not-found-container">
       <h1 className="not-found-heading">Página Não Encontrada</h1>
