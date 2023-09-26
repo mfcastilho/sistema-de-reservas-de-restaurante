5249803a608335cb1a983b0da30e2e182cd9a46a
mfcastilho <fredericocastilho@hotmail.com>
2023-09-25 23:28:56 -0300

    feat: create Error500Page

--- frontend/src/pages/generic-error-page/index.jsx
+++ frontend/src/pages/generic-error-page/index.jsx
@@ -1,14 +1,37 @@
 import './style.css';
+import { useAuth } from "../../components/auth-provider";
+import jwtDecode from 'jwt-decode';
+import { useEffect } from 'react';
 
 const Error500 = () => {
-  return (
-    <div className="error500-container">
-      <h1 className="error500-heading">Erro 500</h1>
-      <p className="error500-message">
-        Ocorreu um erro interno no servidor. Por favor, tente novamente mais tarde.
-      </p>
-    </div>
-  );
+    
+    const { login, logout } = useAuth(); 
+    const token = localStorage.getItem("token");
+    
+
+    const verifyIfIsLogged = ()=>{
+        if(token){
+            const decodedToken = jwtDecode(token);
+            if(decodedToken.exp * 1000 > Date.now()){
+                login();
+            }
+        }else{
+            logout();
+        }   
+    } 
+
+    useEffect(()=>{
+        verifyIfIsLogged();
+    },[]);
+
+    return (
+        <div className="error500-container">
+        <h1 className="error500-heading">Erro 500</h1>
+        <p className="error500-message">
+            Ocorreu um erro interno no servidor. Por favor, tente novamente mais tarde.
+        </p>
+        </div>
+    );
 };
 
 export default Error500;
